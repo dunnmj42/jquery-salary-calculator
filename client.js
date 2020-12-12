e8d48014dc2e7be01568fc28e349ef27135bf97e
Michael Dunn <dunnmj42@gmail.com>
2020-12-11 20:16:13 -0600

    prettier code

--- client.js
+++ client.js
@@ -1,24 +1,24 @@
-console.log('jS loaded');
+console.log("jS loaded");
 
 let employees = [];
 
 $(document).ready(handleReady);
 
 function handleReady() {
-    console.log('jQuery is loaded');
+  console.log("jQuery is loaded");
 
-    renderToDom();
-    calcTotal();
+  renderToDom();
+  calcTotal();
 
-    $('#submitButton').on('click', handleSubmit);
-    $('#employeeList').on('click', '.deleteButton', handleDelete);
+  $("#submitButton").on("click", handleSubmit);
+  $("#employeeList").on("click", ".deleteButton", handleDelete);
 }
 
 function renderToDom() {
-    $('#employeeList').empty();
+  $("#employeeList").empty();
 
-    for (let i = 0; i < employees.length; i++) {
-        $('#employeeList').append(`
+  for (let i = 0; i < employees.length; i++) {
+    $("#employeeList").append(`
             <tr class="employeeRow">
              <td>${employees[i].firstName}</td>
              <td>${employees[i].lastName}</td>
@@ -27,31 +27,28 @@ function renderToDom() {
              <td>${employees[i].annualSalary}</td>
              <td><button id="${i}" class="deleteButton">Delete</button></td>
              </tr>`);
-             
-    }
+  }
 }
 
 function handleSubmit() {
+  console.log("click");
 
-    console.log('click');
-
-    if ($('#firstName').val() === "" || 
-        $('#lastName').val() === "" || 
-        $('#idNumber').val() === "" || 
-        $('#jobTitle').val() === "" || 
-        $('#annualSalary').val() === "") {
-
-        alert('Please make sure you have completed the entire form!')
-    
-    } else {
-
+  if (
+    $("#firstName").val() === "" ||
+    $("#lastName").val() === "" ||
+    $("#idNumber").val() === "" ||
+    $("#jobTitle").val() === "" ||
+    $("#annualSalary").val() === ""
+  ) {
+    alert("Please make sure you have completed the entire form!");
+  } else {
     let newEmployee = {
-        firstName: $('#firstName').val(),
-        lastName: $('#lastName').val(),
-        idNumber: $('#idNumber').val(),
-        jobTitle: $('#jobTitle').val(),
-        annualSalary: $('#annualSalary').val()
-    }
+      firstName: $("#firstName").val(),
+      lastName: $("#lastName").val(),
+      idNumber: $("#idNumber").val(),
+      jobTitle: $("#jobTitle").val(),
+      annualSalary: $("#annualSalary").val(),
+    };
 
     console.log(newEmployee);
     employees.push(newEmployee);
@@ -59,50 +56,47 @@ function handleSubmit() {
     renderToDom();
     clearInputs();
     calcTotal();
-
-    }
+  }
 }
 
 function handleDelete() {
-    console.log('delete click');
+  console.log("delete click");
 
-    let removalTarget = this.id
+  let removalTarget = this.id;
 
-    console.log(removalTarget);
+  console.log("Removal target id is:", removalTarget);
 
-    employees.splice(removalTarget, 1);
+  employees.splice(removalTarget, 1);
 
-    calcTotal();
+  calcTotal();
 
-    $(this).parent().parent().remove();
-
+  $(this).parent().parent().remove();
 }
 
 function calcTotal() {
+  let totalMonthly = 0;
 
-    let totalMonthly = 0;
+  for (let employee of employees) {
+    let monthlySalary = employee.annualSalary / 12;
 
-    for (let employee of employees) {
-        let monthlySalary = (employee.annualSalary / 12);
+    totalMonthly += monthlySalary;
+  }
 
-        totalMonthly += monthlySalary
-    }
-
-    let el = $('#totalMonthly')
-    el.empty();
-    if (totalMonthly <= 20000) {
-        el.append(`<h2 id="totalMonthly">Total Monthly: $ ${totalMonthly}</h2>`)
-    } else {
-        el.append(`<h2 id="totalMonthly" class="overBudget">Total Monthly: $ ${totalMonthly}</h2>`)
-    }
-    
-
+  let el = $("#totalMonthly");
+  el.empty();
+  if (totalMonthly <= 20000) {
+    el.append(`<h2 id="totalMonthly">Total Monthly: $ ${totalMonthly}</h2>`);
+  } else {
+    el.append(
+      `<h2 id="totalMonthly" class="overBudget">Total Monthly: $ ${totalMonthly}</h2>`
+    );
+  }
 }
 
 function clearInputs() {
-    $('#firstName').val('');
-    $('#lastName').val('');
-    $('#idNumber').val('');
-    $('#jobTitle').val('');
-    $('#annualSalary').val('');
+  $("#firstName").val("");
+  $("#lastName").val("");
+  $("#idNumber").val("");
+  $("#jobTitle").val("");
+  $("#annualSalary").val("");
 }
